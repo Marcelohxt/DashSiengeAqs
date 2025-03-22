30bba590097856377e0cfb98c68e7cfff44a2373
marcelohxt <marcelo_hxt@hotmail.com>
2025-03-22 09:03:23 -0300

    Starting App'

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import dcc
 from dash import html
 import pandas as pd
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Carregar seu CSV
 df = pd.read_csv('dados/df_agrupado.csv')  # Substitua 'seu_arquivo.csv' pelo caminho do seu arquivo
@@ -15,7 +16,7 @@ fig = px.scatter(df, x='Gestor_Responsavel', y='Valor_final_negociado_formatado'
 
 # Definir o layout do aplicativo Dash
 app.layout = html.Div([
-    html.H1("Meu Dashboard", style={'text-align': 'center'}),
+    html.H1("Dash Board Suprimentos", style={'text-align': 'center'}),
     dcc.Graph(figure=fig)
 ])
 
