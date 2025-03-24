db0332f882f769a16319927c97b2ea1bc56fd93b
marcelohxt <marcelo_hxt@hotmail.com>
2025-03-24 17:42:16 -0300

    02e

--- app.py
+++ app.py
@@ -36,7 +36,10 @@ fig = px.scatter(df, x='Gestor_Responsavel', y='Valor_final_negociado_formatado'
 app.layout = html.Div([
     html.H1("Dash Board Suprimentos Liliane Amanda", style={'text-align': 'center'}),
     dcc.Graph(figure=fig)
-    from django.conf import settings
+
+
+# levandanto dados em tempo real 
+
 
 ])
 
